6d2381f1f07e01487997c6495608140535fce350
ngrwisnu <adiprayogongrwisnu@gmail.com>
2022-12-20 14:11:33 +0700

    slicing plain html into tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,52 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/detail.css";
+import "../styles/edit-profile.css";
+import "../styles/checkout.css";
+import "../styles/complete-checkout.css";
+import "../styles/homepage.css";
+import "../styles/login.css";
+import "../styles/sign-up.css";
+import "../styles/sign-up-photo.css";
+import "../styles/sign-up-success.css";
+import "../styles/404-not-found.css";
+import "../styles/overview.css";
+import "../styles/sidebar.css";
+import "../styles/transactions.css";
+import "../styles/transactions-detail.css";
+import "../styles/utilities.css";
+import type { AppProps } from "next/app";
+import { Fragment } from "react";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Fragment>
+      <Head>
+        {/* <!-- Bootstrap CSS --> */}
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x"
+          crossOrigin="anonymous"
+        />
+
+        {/* <!-- Google Font --> */}
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+
+        {/* <!-- AOS Animation --> */}
+        <link
+          href="https://unpkg.com/aos@2.3.1/dist/aos.css"
+          rel="stylesheet"
+        />
+
+        {/* <!-- Calling Bootstrap --> */}
+        <script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js"
+          integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4"
+          crossOrigin="anonymous"
+          defer
+        ></script>
+      </Head>
+      <Component {...pageProps} />
+    </Fragment>
+  );
 }
---
+++ pages/detail/[id].tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const gameDetail = () => {
+  return <div>gameDetail</div>;
+};
+
+export default gameDetail;
