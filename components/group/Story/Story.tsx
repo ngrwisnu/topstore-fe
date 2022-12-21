9bb5ef1f77ccd61383842db41d26059d82ab5d3a
ngrwisnu <adiprayogongrwisnu@gmail.com>
2022-12-21 19:12:25 +0700

    create story section component

--- components/group/Story/Story.tsx
+++ components/group/Story/Story.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 const Story = () => {
@@ -9,10 +10,10 @@ const Story = () => {
             className="col-lg-7 col-12 d-lg-flex d-none justify-content-lg-end pe-lg-60"
             data-aos="zoom-in"
           >
-            <img
+            <Image
               src="/img/Header-9.png"
-              width="612"
-              height="452"
+              width={612}
+              height={452}
               className="img-fluid"
               alt=""
             />
