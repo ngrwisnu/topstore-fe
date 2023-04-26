7dab077987d82482f458898ac995b71e5a4d703f
ngrwisnu <adiprayogongrwisnu@gmail.com>
2023-04-27 03:42:35 +0700

    fix: use data from API to show user profile in the sidebar

--- components/group/Sidebar/Profile.tsx
+++ components/group/Sidebar/Profile.tsx
@@ -1,18 +1,23 @@
 import Image from "next/image";
 import React from "react";
+import { loggedInUser } from "../../../zustand";
 
 const Profile = () => {
+  const user = loggedInUser((state: any) => state.data);
+
   return (
     <div className="user text-center pb-50 pe-30">
-      <Image
-        src="/img/avatar-1.png"
-        width={90}
-        height={90}
-        alt=""
-        className="img-fluid mb-20"
-      />
-      <h2 className="fw-bold text-xl color-palette-1 m-0">Shayna Anne</h2>
-      <p className="color-palette-2 m-0">shayna@anne.com</p>
+      <div className="profile-img-wrapper">
+        <Image
+          src={user.image}
+          width={90}
+          height={90}
+          alt="profile"
+          className="img-fluid mb-20"
+        />
+      </div>
+      <h2 className="fw-bold text-xl color-palette-1 m-0">{user.fullname}</h2>
+      <p className="color-palette-2 m-0">{user.email}</p>
     </div>
   );
 };
